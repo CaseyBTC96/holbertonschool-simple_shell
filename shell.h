3761f80aa25ecdc8c09eda3a4c86372066e50ecd
CaseyBTC96 <144287360+CaseyBTC96@users.noreply.github.com>
2023-12-05 15:35:20 -0600

    Update shell.h

--- shell.h
+++ shell.h
@@ -3,4 +3,11 @@
 char* find_file(char *pattern, char *directoryPath);
 char* abs_path(char *path, char *file);
 void free_array(char *array);
+/* built in functions */
+int change_directory(char **args, char *input_stdin, int *exit_status);
+int _setenv(char **args, char *input_stdin, int *exit_status);
+int _unsetenv(char **args, char *input_stdin, int *exit_status);
+int _env(char **args, char *input_stdin, int *exit_status);
+int _exit(char **args, *input_stdin, int *exit_status);
+
 #endif 
