45c33c79061ad8c0ebd305eb2bab948442f1b738
CaseyBTC96 <144287360+CaseyBTC96@users.noreply.github.com>
2023-12-06 14:37:50 -0600

    Update shell.h

--- shell.h
+++ shell.h
@@ -3,6 +3,18 @@
 char* find_file(char *pattern, char *directoryPath);
 char* abs_path(char *path, char *file);
 void free_array(char *array);
+/**
+ * struct choose_builtin - Builtin commands struct
+ * @name_builin: Name of builtin command
+ * @func_builtin: Pointer to builtin command functions
+ */
+typedef struct choose_builtin
+{
+        char *name_builtin;
+        int (*func_builtin)(char **, char *, int *);
+
+} choose_builtins_t;
+
 /* built in functions */
 int change_directory(char **args, char *input_stdin, int *exit_status);
 int _setenv(char **args, char *input_stdin, int *exit_status);
