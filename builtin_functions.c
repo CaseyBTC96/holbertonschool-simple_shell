bf2f03e6e8c78722712c977d6799611ada0217a3
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-07 13:23:50 -0800

    New if statements

--- builtin_functions.c
+++ builtin_functions.c
@@ -8,53 +8,11 @@
  * @exit_status: exit status
  * Return: Always 1 (success)
  */
-int _env(char **args, char *input_stdin, int *exit_status)
+void _env(void)
 {
-	int i = 0;
-
-	(void)args;
-	(void)input_stdin;
-	(void)exit_status;
-
-	if (environ[i] == NULL)
-	{
-		printf("%s", "The built in env is empty");
-		return (1);
-	}
-	for (i = 0; environ[i]; i++)
-		printf("%s\n", environ[i]);
-
-	return (1);
+  extern char **environ;
+  
+  for (char **env = environ; *env != NULL; env++)
+		printf("%s\n", *env);
 }
-
-/**
- * _exit - Function exit built in
- * @args: arguments
- * @input_stdin: input of stdin
- * @exit_status: exit status
- * Return: Exit success
- */
-int _exit(char **argss, char *input_stdin, int *exit_status)
-{
-	int ouput_exit = 0;
-
-	(void)args;
-	(void)input_stdin;
-	(void)exit_status;
-
-	if (args[1] == NULL)
-	{
-		free(args);
-		free(input_stdin);
-		exit(*exit_status);
-	}
-	if (ags[2] != NULL)
-	{
-		fprintf(stderr, "exit: too many arguments\n");
-		return (0);
-	}
-	output_exit = atoi(args[1]);
-	free(args);
-	free(input_stdin);
-	exit(output_exit);
-}
+       
--- shell.h
+++ shell.h
@@ -13,6 +13,6 @@ typedef struct choose_builtin
 } choose_builtins_t;
 
 /* built in functions */
-int _env(char **args, char *input_stdin, int *exit_status);
+void _env(void);
 int _execute(char *file, char *const args[], char *const env[]);
 #endif 
