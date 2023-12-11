5583414b519b9bdfd8cfee327deb2103469c4f69
Casey Paul <7227@holbertonstudents.com>
2023-12-11 08:46:30 -0800

    functions env() and exit()

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,18 +1,60 @@
-#include "shell.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- * _env - Function that print environment
+ * hsh_env - Function that print enviromental
  * @args: arguments
  * @input_stdin: input of stdin
  * @exit_status: exit status
  * Return: Always 1 (success)
  */
-void _env(void)
+int hsh_env(char **args, char *input_stdin, int *exit_status)
 {
-  extern char **environ;
-  char **env;
-  for (env = environ; *env != NULL; env++)
-		printf("%s\n", *env);
+	int i = 0;
+
+	(void)args;
+	(void)input_stdin;
+	(void)exit_status;
+
+
+	if (environ[i] == NULL)
+	{
+		printf("%s", "The built in env is empty");
+		return (1);
+	}
+	for (i = 0; environ[i]; i++)
+		printf("%s\n", environ[i]);
+
+	return (1);
 }
-       
+
+/**
+ * hsh_exit - Function exit built in
+ * @args: arguments
+ * @input_stdin: input of stdin
+ * @exit_status: exit status
+ * Return: Exit success
+ */
+int hsh_exit(char **args, char *input_stdin, int *exit_status)
+{
+	int output_exit = 0;
+
+	(void)args;
+	(void)input_stdin;
+	(void)exit_status;
+
+	if (args[1] == NULL)
+	{
+		free(args);
+		free(input_stdin);
+		exit(*exit_status);
+	}
+	if (args[2] != NULL)
+	{
+		fprintf(stderr, "exit: too many arguments\n");
+		return (0);
+	}
+	output_exit = atoi(args[1]);
+	free(args);
+	free(input_stdin);
+	exit(output_exit);
+}
